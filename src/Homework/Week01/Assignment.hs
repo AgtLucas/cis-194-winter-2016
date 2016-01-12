ee0ccd9517748738a36b52236b9415e912abacc9
InfinityGone <InfinityGone@users.noreply.github.com>
2016-01-11 21:15:03 -0800

    Week 1 solutions.

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,30 +2,49 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits 0 = []
+toDigits x
+    | x < 0     = []
+    | otherwise = map (\n -> read [n]::Integer) (show x)
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev = reverse . toDigits
 
 -- #2
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther = reverse . doubleLeftEveryOther . reverse
+
+-- double every other number starting from the left
+-- e.g. [1,2,3,4] -> [1,4,3,8]
+doubleLeftEveryOther :: [Integer] -> [Integer]
+doubleLeftEveryOther [] = []
+doubleLeftEveryOther (x:[]) = [x]
+doubleLeftEveryOther (x:y:zs) = x : (y*2) : (doubleLeftEveryOther zs)
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits x = foldl (+) 0 (singleDigits x)
+
+-- make sure we have a list with a single digits
+-- e.g. [1,32,4] -> [1,3,2,4]
+singleDigits :: [Integer] -> [Integer]
+singleDigits x = concat (map toDigits x)
 
 -- #4
 validate :: Integer -> Bool
-validate = undefined
+validate x =
+    let sum = (sumDigits . doubleEveryOther . toDigits) x
+    in sum `mod` 10 == 0
 
 -- #5
 type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _ = []
+hanoi 1 a b _ = [(a, b)]
+hanoi d a b c = (hanoi (d-1) a c b) ++ [(a, b)] ++ (hanoi (d-1) c b a)
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 = undefined
