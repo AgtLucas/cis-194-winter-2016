aa834253948e4acd21961ecc9686a2cff88e4cee
InfinityGone <InfinityGone@users.noreply.github.com>
2016-01-20 22:28:01 -0800

    First 3 exercises of week 2

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -15,19 +15,31 @@ import Homework.Week02.Log
 
 -- #1a
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage = parseMessageWords . words
+
+parseMessageWords :: [String] -> LogMessage
+parseMessageWords ("E":severity:timestamp:xs) = LogMessage (Error (read severity)) (read timestamp) (unwords xs)
+parseMessageWords ("I":timestamp:xs) = LogMessage Info (read timestamp) (unwords xs)
+parseMessageWords ("W":timestamp:xs) = LogMessage Warning (read timestamp) (unwords xs)
+parseMessageWords a = Unknown $ unwords a
+--
+
 
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse line = map parseMessage (lines line)
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert (Unknown str) tree = tree -- unknown just returns the tree
+insert m (Leaf) = Node Leaf m Leaf
+insert m@(LogMessage _ timestamp _) (Node l root@(LogMessage _ rootTimestamp _) r)
+    | timestamp < rootTimestamp = (Node (insert m l) root r)
+    | otherwise = (Node l root (insert m r))
 
 -- #3
 build :: [LogMessage] -> MessageTree
-build = undefined
+build l = foldl (\acc m -> insert m acc) Leaf l
 
 -- #4
 inOrder :: MessageTree -> [LogMessage]
