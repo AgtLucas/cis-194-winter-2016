9defe367906f40ece9046d1677ff1106b947f740
InfinityGone <InfinityGone@users.noreply.github.com>
2016-02-08 19:34:36 -0800

    Week 4

--- src/Homework/Week04/Assignment.hs
+++ src/Homework/Week04/Assignment.hs
@@ -20,71 +20,94 @@ module Homework.Week04.Assignment (
 ) where
 
 import Homework.Week04.BST
+import Data.Char
+import Data.List
+import Data.Maybe
 
 -- #1
+-- a cannot influence b's type, so b = b
 ex1 :: a -> b -> b
-ex1 = undefined
+ex1 _ b = b
 
 -- #2
+-- must return either the first or second arg...
 ex2 :: a -> a -> a
-ex2 = undefined
+ex2 a _ = a
+--ex2 _ b = b
 
 -- #3
+-- Can only return a
 ex3 :: Int -> a -> a
-ex3 = undefined
+ex3 _ x = x
 
 -- #4
+-- Finite - Bool could cause return of second or third arg, but thats it
 ex4 :: Bool -> a -> a -> a
-ex4 = undefined
+ex4 True a _ = a
+ex4 False _ b = b
 
 -- #5
+-- Either id or not based on first arg
+-- or const true or false
 ex5 :: Bool -> Bool
-ex5 = undefined
+ex5 = id
 
 -- #6
 ex6 :: (a -> a) -> a
-ex6 = undefined
+ex6 = error "impossible"
 
 -- #7
+-- two: return the second arg, or the result of first(second)
 ex7 :: (a -> a) -> a -> a
-ex7 = undefined
+ex7 fn x = fn x
+--ex7 _ x = x
 
 -- #8
 ex8 :: [a] -> [a]
-ex8 = undefined
+ex8 = id
 
 -- #9
 ex9 :: (a -> b) -> [a] -> [b]
-ex9 = undefined
+ex9 = map
 
 -- #10
 ex10 :: Maybe a -> a
-ex10 = undefined
+ex10 = error "impossible"
+-- fromJust works, but would throw an error when it receices Nothing
 
 -- #11
 ex11 :: a -> Maybe a
-ex11 = undefined
+ex11 = Just
 
 -- #12
 ex12 :: Maybe a -> Maybe a
-ex12 = undefined
+ex12 = id
 
 -- #13
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST = undefined
+insertBST  _ a Leaf = Node Leaf a Leaf
+insertBST  order a (Node left root right)
+    | order a root == LT = Node (insertBST order a left) root right
+    | otherwise = Node right root (insertBST order a right)
 
 -- #14
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+
 allCaps :: [String] -> Bool
-allCaps = undefined
+allCaps = all (maybe False isUpper . safeHead)
 
 -- #15
 dropTrailingWhitespace :: String -> String
-dropTrailingWhitespace = undefined
+dropTrailingWhitespace = dropWhileEnd isSpace
 
 -- #16
 firstLetters :: [String] -> [Char]
-firstLetters = undefined
+firstLetters = mapMaybe safeHead
 
 -- #17
 asList :: [String] -> String
-asList = undefined
+asList xs = concat $ ["["] ++ (intersperse ", " xs) ++ ["]"]
